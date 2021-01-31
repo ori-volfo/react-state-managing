cb58d3b2b56ce76693821f70b9910a668940ca9a
OriV <oriv@aspireglobal.com>
2021-01-31 17:32:13 +0200

    add react router add more routes

--- 04/demos/before/src/App.jsx
+++ 04/demos/before/src/App.jsx
@@ -3,16 +3,24 @@ import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import Products from './Products';
+import { Routes, Route } from 'react-router-dom';
+import Detail from "./Detail";
+import Cart from "./Cart";
+
 export default function App() {
-  return (
-      <>
-        <div className="content">
-          <Header />
-          <main>
-            <Products/>
-          </main>
-        </div>
-        <Footer />
-      </>
-  );
+    return (
+        <>
+            <div className="content">
+                <Header/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Products/>}/>
+                        <Route path="/detail" element={<Detail/>}/>
+                        <Route path="/cart" element={<Cart/>}/>
+                    </Routes>
+                </main>
+            </div>
+            <Footer/>
+        </>
+    );
 }
