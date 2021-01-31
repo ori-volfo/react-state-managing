8d79d5b444a3e1d668b1fe268eda92e79739380f
OriV <oriv@aspireglobal.com>
2021-01-31 17:39:03 +0200

    react router useParams

--- 04/demos/before/src/Products.jsx
+++ 04/demos/before/src/Products.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from "react";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
+import { useParams } from 'react-router-dom';
 
 export default function Products() {
     const [size, setSize] = useState('');
-    const { data: products, loading, error } = useFetch("products?category=shoes");
+    const { category } = useParams();
+    const { data: products, loading, error } = useFetch("products?category=" + category);
 
     function renderProduct(p) {
         return (
