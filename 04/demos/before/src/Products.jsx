3e413dd60f385543c667b6ba5c0e172e599176b4
OriV <oriv@aspireglobal.com>
2021-02-01 09:34:55 +0200

    add PageNotFound

--- 04/demos/before/src/Products.jsx
+++ 04/demos/before/src/Products.jsx
@@ -2,10 +2,12 @@ import React, {useState} from "react";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
 import { useParams } from 'react-router-dom';
+import PageNotFound from "./PageNotFound";
 
 export default function Products() {
     const [size, setSize] = useState('');
     const { category } = useParams();
+
     const { data: products, loading, error } = useFetch("products?category=" + category);
 
     function renderProduct(p) {
@@ -24,6 +26,7 @@ export default function Products() {
 
     if (error) throw error;
     if(loading) return <Spinner/>;
+    if(products.length === 0) return <PageNotFound/>
     return (
         <>
             <section id="filters">
