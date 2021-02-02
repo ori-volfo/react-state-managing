20cc438c50876fadfc88256c64175db963e24cb1
OriV <oriv@aspireglobal.com>
2021-02-02 13:20:16 +0200

    select state

--- 05/demos/before/src/Detail.jsx
+++ 05/demos/before/src/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
@@ -8,7 +8,7 @@ export default function Detail() {
   const { id } = useParams();
   const navigate = useNavigate();
   const { data: product, loading, error } = useFetch(`products/${id}`);
-
+  const [sku, setSku] = useState('');
   if (loading) return <Spinner />;
   if (!product) return <PageNotFound />;
   if (error) throw error;
@@ -18,8 +18,16 @@ export default function Detail() {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+        <select
+            id="size"
+            value={sku}
+            onChange={(e) => setSku(e.target.value)}
+        >
+            <option value="">What size?</option>
+            {product.skus.map(item => <option key={item.sku} value={item.sku}>{item.size}</option>)}
+        </select>
       <p>
-        <button className="btn btn-primary" onClick={() => navigate("/cart")}>
+        <button disabled={!sku} className="btn btn-primary" onClick={() => navigate("/cart")}>
           Add to cart
         </button>
       </p>
