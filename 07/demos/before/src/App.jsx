b867547a03ddfc38d7994813192b31980ea670e0
OriV <oriv@aspireglobal.com>
2021-02-08 17:15:56 +0200

    useRef
    replace useState

--- 07/demos/before/src/App.jsx
+++ 07/demos/before/src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
 import { Routes, Route } from "react-router-dom";
-import Detail from "./Detail";
+import Detail from "./DetailRefs";
 import Cart from "./Cart";
 import Checkout from "./Checkout";
 
