fae36d02b48e7312199324cbe9e27ad74afb4d01
OriV <oriv@aspireglobal.com>
2021-02-10 13:54:36 +0200

    useRef
    storing previous value

--- 07/demos/before/src/services/useFetchAll.js
+++ 07/demos/before/src/services/useFetchAll.js
@@ -1,11 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function useFetchAll(urls) {
+  const prevUrls = useRef([]);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    if ( areEqual( prevUrls.current, urls ) ) {
+        setLoading(false);
+        return
+    }
+    prevUrls.current = urls;
     const promises = urls.map((url) =>
       fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
         if (response.ok) return response.json();
@@ -20,8 +26,14 @@ export default function useFetchAll(urls) {
         setError(e);
       })
       .finally(() => setLoading(false));
-    // eslint-disable-next-line
   }, []);
 
   return { data, loading, error };
+}
+
+function areEqual( arr1, arr2 ) {
+    return (
+        arr1.length === arr2.length &&
+            arr1.every(( val, i ) => val === arr2[i])
+    );
 }
