c85851b709283fc6bc5560f4a1a3d044674b966a
OriV <oriv@aspireglobal.com>
2021-02-11 09:12:56 +0200

    useReducer moving logics to cartReducer

--- 08/demos/before/src/cartReducer.jsx
+++ 08/demos/before/src/cartReducer.jsx
@@ -4,7 +4,7 @@ export default function cartReducer( cart, action ) {
     switch ( action.type ) {
         case 'empty':
             return [];
-        case 'add':
+        case 'add': {
             const { id, sku } = action;
             const itemInCart = cart.find((i) => i.sku === sku);
             if (itemInCart) {
@@ -16,7 +16,13 @@ export default function cartReducer( cart, action ) {
                 // Return new array with the new item appended
                 return [...cart, { id, sku, quantity: 1 }];
             }
-
+        }
+        case 'update': {
+            const { sku, quantity } = action;
+            return quantity === 0
+                ? cart.filter((i) => i.sku !== sku)
+                : cart.map((i) => (i.sku === sku ? {...i, quantity} : i));
+        }
         default:
             throw new Error('Unhandled action ' + action.type);
     }
