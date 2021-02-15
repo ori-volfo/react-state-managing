f0acf33858b9ebc01ad7a7f4110f2015e01d8f14
OriV <oriv@aspireglobal.com>
2021-02-15 14:13:31 +0200

    creat cartContext part 4 - add error

--- 09/demos/before/src/cartContext.jsx
+++ 09/demos/before/src/cartContext.jsx
@@ -20,5 +20,8 @@ export function CartProvider(props) {
 
 export function useCart() {
     const context = useContext(CartContext);
+    if(!context){
+        throw new Error("useCart must be used within CartProvider. wrap a parent component in <CartProvider> to fix this error");
+    }
     return context;
 }
