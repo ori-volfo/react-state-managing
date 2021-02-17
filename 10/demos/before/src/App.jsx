19745059d8aedd0b9ba2230fe85e5cff90fe1c73
OriV <oriv@aspireglobal.com>
2021-02-17 16:32:54 +0200

    Converting Checkout function component to a class component

--- 10/demos/before/src/App.jsx
+++ 10/demos/before/src/App.jsx
@@ -6,9 +6,12 @@ import Products from "./Products";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
 import Cart from "./Cart";
-import Checkout from "./Checkout";
+import Checkout from "./Checkout.class";
+import { useCart } from "./cartContext";
+
 
 export default function App() {
+  const { dispatch } = useCart();
   return (
     <>
       <div className="content">
@@ -19,7 +22,7 @@ export default function App() {
             <Route path="/:category" element={<Products />} />
             <Route path="/:category/:id" element={<Detail />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/checkout" element={<Checkout dispatch={dispatch}/>} />
           </Routes>
         </main>
       </div>
