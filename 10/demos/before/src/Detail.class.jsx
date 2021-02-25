a793da1238a5c76b60941dc85ba223c50caee876
OriV <oriv@aspireglobal.com>
2021-02-25 15:56:50 +0200

    Consuming multiple contexts

--- 10/demos/before/src/Detail.class.jsx
+++ 10/demos/before/src/Detail.class.jsx
@@ -14,52 +14,55 @@ class Detail extends React.Component{
     state = {
         sku: ''
     }
-    static contextType = CartContext;
 
     render() {
         const { id, navigate } = this.props;
         const { sku } = this.state;
 
         return (
-            <Fetch url={`products/${id}`} >
-                { (product, loading, error) => {
+            <CartContext className="Consumer">
+                {({dispatch})=> {
+                    return <Fetch url={`products/${id}`} >
+                        { (product, loading, error) => {
 
-                if (loading) return <Spinner />;
-                if (!product) return <PageNotFound />;
-                if (error) throw error;
+                            if (loading) return <Spinner />;
+                            if (!product) return <PageNotFound />;
+                            if (error) throw error;
 
-                return (
-                    <div id="detail">
-                        <h1>{product.name}</h1>
-                        <p>{product.description}</p>
-                        <p id="price">${product.price}</p>
+                            return (
+                                <div id="detail">
+                                    <h1>{product.name}</h1>
+                                    <p>{product.description}</p>
+                                    <p id="price">${product.price}</p>
 
-                        <select id="size" value={sku} onChange={(e) => this.setState({sku: e.target.value})}>
-                            <option value="">What size?</option>
-                            {product.skus.map((s) => (
-                                <option key={s.sku} value={s.sku}>
-                                    {s.size}
-                                </option>
-                            ))}
-                        </select>
+                                    <select id="size" value={sku} onChange={(e) => this.setState({sku: e.target.value})}>
+                                        <option value="">What size?</option>
+                                        {product.skus.map((s) => (
+                                            <option key={s.sku} value={s.sku}>
+                                                {s.size}
+                                            </option>
+                                        ))}
+                                    </select>
 
-                        <p>
-                            <button
-                                disabled={!sku}
-                                className="btn btn-primary"
-                                onClick={() => {
-                                    this.context.dispatch({ type: "add", id, sku });
-                                    navigate("/cart");
-                                }}
-                            >
-                                Add to cart
-                            </button>
-                        </p>
-                        <img src={`/images/${product.image}`} alt={product.category} />
-                    </div>
-                )
-            }}
-        </Fetch>
+                                    <p>
+                                        <button
+                                            disabled={!sku}
+                                            className="btn btn-primary"
+                                            onClick={() => {
+                                                this.context.dispatch({ type: "add", id, sku });
+                                                navigate("/cart");
+                                            }}
+                                        >
+                                            Add to cart
+                                        </button>
+                                    </p>
+                                    <img src={`/images/${product.image}`} alt={product.category} />
+                                </div>
+                            )
+                        }}
+                    </Fetch>
+                }}
+            </CartContext>
     )
     }
 }
